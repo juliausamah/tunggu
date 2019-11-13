fa21bf0c4aff1fa93d83e9afe6571259fd24933c
juliausamah <57596690+juliausamah@users.noreply.github.com>
2019-11-13 16:25:26 +0700

    Update pemanggil.js

--- pemanggil.js
+++ pemanggil.js
@@ -22,7 +22,7 @@ if(a_to_vh==false)
 var x=Math.floor((Math.random()*2)+ 1);
 var xxx=null;
 if(x=="1"){xxx="/2019/11/what-is-insurance.html"}
-if(x=="2"){xxx="/2019/11/what-is-health-insurance.html"}a_to_ve[i].href="https://tipsku05.blogspot.com"+xxx+"?url="+ encryptedUrl;a_to_ve[i].rel="nofollow";a_to_vb++;a_to_vc+=i+":::"+ a_to_ve[i].href+"\n";}}
+if(x=="2"){xxx="/2019/11/what-is-health-insurance.html"}a_to_ve[i].href="https://batukarutpedia.blogspot.com"+xxx+"?url="+ encryptedUrl;a_to_ve[i].rel="nofollow";a_to_vb++;a_to_vc+=i+":::"+ a_to_ve[i].href+"\n";}}
 var a_to_vj=document.getElementById("anonyminized");var a_to_vk=document.getElementById("found_links");if(a_to_vj)
 {a_to_vj.innerHTML+=a_to_vb;}
 if(a_to_vk)
